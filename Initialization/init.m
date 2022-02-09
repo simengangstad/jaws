ae3312c567c9a5d47f2bd858971307eb1d5f10c8
Ziyue1234 <89876193+Ziyue1234@users.noreply.github.com>
2022-02-09 12:32:53 +0100

    Add files via upload

---
+++ Initialization/init.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% init()                                                                  %
+%                                                                         %              
+% Set initial parameters for part1.slx and part2.slx                      %
+%                                                                         %
+% Created:      2018.07.12	Jon Bjørnø                                    %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all;
+
+load('supply.mat');
+load('supplyABC.mat');
+load('thrusters_sup.mat')
+
+% Initial position x, y, z, phi, theta, psi
+eta0 = [0,0,0,0,0,0]';
+% Initial velocity u, v, w, p, q, r
+nu0 = [0,0,0,0,0,0]';
