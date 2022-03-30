f22b2b9b7757b6988b8e31b060e7df31e606c75c
Simen Gangstad <simen.gangstad@me.com>
2022-03-30 14:26:04 +0200

    Clean up

---
+++ src/Functions/Legacy/main.m
@@ -0,0 +1,218 @@
+clear;
+
+parameters = construct_parameters();
+
+%% Waypoints & spline
+
+[x, y, K] = generate_waypoints();
+waypoints = [x; y];
+waypoint_index = 1;
+next_waypoints = [];
+
+%% Setup
+h  = parameters.simulation.step_size;       % sampling time [s]
+total_time = 3 * 3600;
+Ns = total_time / h;                        % no. of samples
+
+U_ref = 1.5;                                % desired surge speed (m/s)
+
+% Initial states
+eta = [0 0 pi / 2 + 0.2]';
+nu  = [0 0 0]';
+delta = 0;
+n = 0;
+Qm = 0;
+x = [nu' eta' delta n Qm]';
+
+% Path following parameters
+look_ahead_distance = parameters.guidance.look_ahead;
+kappa               = parameters.guidance.kappa;
+
+% Heading reference model & control
+psi_ref_x = [0; 0; 0];
+e_psi_int = 0;
+y_int = 0;
+
+% Speed control
+e_u_int = 0;
+
+% Wind expressed in NED
+Vw = 1;
+betaVw = deg2rad(135);
+rho_a = 1.247;
+cy = 0.95;
+cn = 0.15;
+A_Lw = 10 * parameters.ship.length;
+
+% Current
+Vc = 0.2;
+betaVc = deg2rad(45);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MAIN LOOP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+simdata = zeros(Ns+1, length(x) + 5);       % table of simulation data
+
+for i=1:Ns+1
+    
+    t = (i-1) * h;              % time (s)
+
+    %% Current disturbance
+
+    uc = Vc * cos(betaVc - x(6)); 
+    vc = Vc * sin(betaVc - x(6)); 
+    nu_c = [uc vc 0]';
+
+    %% Wind disturbance
+
+    % Wind velocity in world frame
+    uw = Vw * cos(betaVw - x(6)); 
+    vw = Vw * sin(betaVw - x(6)); 
+    
+    % These are relative to the boat's current velocity
+    u_rw = x(1) - uw;
+    v_rw = x(2) - vw;
+    V_rw = sqrt(u_rw^2 + v_rw^2); 
+    gamma_rw = -atan2(v_rw, u_rw);
+    
+    Ywind = 0.5 * rho_a * V_rw^2 * cy * sin(gamma_rw) * A_Lw;
+    Nwind = 0.5 * rho_a * V_rw^2 * cn * sin(2 * gamma_rw) * A_Lw * parameters.ship.length;
+
+    tau_wind = [0 Ywind Nwind]';
+
+
+    %% Guidance law
+    [current_waypoint, waypoint_index] = closest_waypoint(x(4:5), waypoints, waypoint_index);
+    
+%     radius = 1000 * (1 - 175 * K(waypoint_index));
+%
+%     [next_waypoint, found] = find_farthest_point_spanned_by_circle(radius, waypoints, waypoint_index);
+% 
+%     if true
+%         next_waypoint = waypoints(:, waypoint_index + 1)';
+%     end
+    
+    next_waypoint = waypoints(:, waypoint_index + 1)';
+
+    [e_y, pi_p] = cross_track_error(next_waypoint(1), ...
+                                    next_waypoint(2), ...
+                                    current_waypoint(1), ...
+                                    current_waypoint(2), ...
+                                    x(4), ...
+                                    x(5));
+
+    look_ahead = look_ahead_distance * (1 - 150 * K(waypoint_index));
+
+    [psi_desired, y_int_dot] = ilos_guidance(e_y, pi_p, y_int, look_ahead, kappa);
+    psi_ref = psi_desired;
+    
+    next_waypoints = [next_waypoints; next_waypoint];
+
+    %% Heading control
+    % The derivative coming from the reference model is the state
+    % derivative of the reference model
+    psi_ref_x_dot = heading_reference_model(psi_ref_x, psi_ref);
+
+    % We can the extract the desired state for heading (psi)
+    % and heading rate (r) from the reference model
+    psi_desired = psi_ref_x(1);
+    r_desired = psi_ref_x(2);
+
+    % Control law
+    e_psi = ssa(x(6) - psi_desired);
+    e_r = ssa(x(3) - r_desired);
+
+    % Now we retrieve the rudder angle command from the heading PID
+    delta_c = heading_pid(e_psi, e_r, e_psi_int, parameters.nomoto); 
+
+    % Anti-integrator windup
+    if abs(delta_c) > parameters.rudder.max_angle
+       delta_c = parameters.rudder.max_angle * sign(delta_c);
+       e_psi_int = e_psi_int - h * e_psi;
+    end
+
+    %% Speed control
+    u_desired = U_ref + 300 * K(waypoint_index);
+    e_u = x(1) - u_desired;
+
+    % Here n_c is the rps of the motor (n) commanded 
+    n_c = speed_pid(u_desired, e_u, e_u_int, parameters);
+
+    %% Ship dynamics
+    u = [delta_c n_c]';
+    [x_dot, u] = supply_model(x, u, nu_c, tau_wind, parameters);
+    
+    % Store simulation data in a table (for testing)
+    simdata(i,:) = [t x(1:3)' x(4:6)' x(7) x(8) u(1) u(2)...
+                    u_desired psi_desired r_desired];     
+ 
+    %% Euler integration
+    % Make integration in batch to save some time
+    s     = [x; psi_ref_x; e_psi_int; e_u_int; y_int];
+    s_dot = [x_dot; psi_ref_x_dot; e_psi; e_u; y_int_dot];
+    
+    s = euler2(s_dot, s, h);
+
+    x = s(1:9);
+    psi_ref_x = s(10:12);
+    e_psi_int = s(13);
+    e_u_int = s(14);
+    y_int = s(15);
+
+    if mod(i, 3600 / h) == 0
+        hour = i * h / 3600;
+        fprintf("Done simulating %i/%.1f hours\n", hour, total_time / 3600.0);
+    end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PLOTS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t       = simdata(:,1);                 % s
+u       = simdata(:,2);                 % m/s
+v       = simdata(:,3);                 % m/s
+r       = (180/pi) * simdata(:,4);      % deg/s
+x       = simdata(:,5);                 % m
+y       = simdata(:,6);                 % m
+psi     = (180/pi) * simdata(:,7);      % deg
+delta   = (180/pi) * simdata(:,8);      % deg
+n       = 60 * simdata(:,9);            % rpm
+delta_c = (180/pi) * simdata(:,10);     % deg
+n_c     = 60 * simdata(:,11);           % rpm
+u_d     = simdata(:,12);                % m/s
+psi_d   = (180/pi) * simdata(:,13);     % deg
+r_d     = (180/pi) * simdata(:,14);     % deg/s
+
+
+figure(1)
+figure(gcf)
+subplot(311)
+plot(y,x,'linewidth',2); axis('equal')
+title('North-East positions (m)'); xlabel('(m)'); ylabel('(m)'); 
+subplot(312)
+plot(t,psi,t,psi_d,'linewidth',2);
+title('Actual and desired yaw angles (deg)'); xlabel('time (s)');
+legend(["Actual", "Desired"]);
+subplot(313)
+plot(t,r,t,r_d,'linewidth',2);
+title('Actual and desired yaw rates (deg/s)'); xlabel('time (s)');
+legend(["Actual", "Desired"]);
+
+
+figure(2)
+figure(gcf)
+subplot(311)
+plot(t,u,t,u_d,'linewidth',2);
+title('Actual and desired surge velocities (m/s)'); xlabel('time (s)');
+legend(["Actual", "Desired"]);
+subplot(312)
+plot(t,n,t,n_c,'linewidth',2);
+title('Actual and commanded propeller speed (rpm)'); xlabel('time (s)');
+legend(["Actual", "Desired"]);
+subplot(313)
+plot(t,delta,t,delta_c,'linewidth',2);
+title('Actual and commanded rudder angles (deg)'); xlabel('time (s)');
+legend(["Actual", "Desired"]);
+
+pathplotter(x, y, waypoints, next_waypoints');
--- src/main.m
+++ src/main.m
@@ -1,218 +1,100 @@
 clear;
 
-parameters = construct_parameters();
+%% Parameters and data
+load('supply.mat');
+load('supplyABC.mat');
+load('thrusters_sup.mat');
+load('wind_coeff3.mat');
+load('net_modeling.mat');
 
-%% Waypoints & spline
+parameters                              = construct_parameters();
+[waypoints_x, waypoints_y, waypoints_K] = generate_waypoints();
+waypoints                               = [waypoints_x; waypoints_y];
 
-[x, y, K] = generate_waypoints();
-waypoints = [x; y];
-waypoint_index = 1;
-next_waypoints = [];
+%% Simulation
 
-%% Setup
-h  = parameters.simulation.step_size;       % sampling time [s]
-total_time = 3 * 3600;
-Ns = total_time / h;                        % no. of samples
+output = sim('model', 4 * 3600);
 
-U_ref = 1.5;                                % desired surge speed (m/s)
+timespan = output.vessel1_data.Time;
 
-% Initial states
-eta = [0 0 pi / 2 + 0.2]';
-nu  = [0 0 0]';
-delta = 0;
-n = 0;
-Qm = 0;
-x = [nu' eta' delta n Qm]';
+data_vessel_1 = output.vessel1_data.Data;
+data_vessel_2 = output.vessel2_data.Data;
 
-% Path following parameters
-look_ahead_distance = parameters.guidance.look_ahead;
-kappa               = parameters.guidance.kappa;
+%% Plot kinematic and actuator data
+x1       = data_vessel_1(:, 1);
+y1       = data_vessel_1(:, 2);
+x2       = data_vessel_2(:, 1);
+y2       = data_vessel_2(:, 2);
+x_offset = data_vessel_2(:, 7);
+y_offset = data_vessel_2(:, 8);
 
-% Heading reference model & control
-psi_ref_x = [0; 0; 0];
-e_psi_int = 0;
-y_int = 0;
+speed_vessel_1 = sqrt(data_vessel_1(:, 4).^2 + data_vessel_1(:, 5).^2);
+speed_vessel_2 = sqrt(data_vessel_2(:, 4).^2 + data_vessel_2(:, 5).^2);
 
-% Speed control
-e_u_int = 0;
+rudder_angle_vessel_1               = data_vessel_1(:, 10);
+propeller_speed_vessel_1            = data_vessel_1(:, 11) * 60;
+rudder_angle_commanded_vessel_1     = data_vessel_1(:, 12);
+propeller_speed_commanded_vessel_1  = data_vessel_1(:, 13) * 60;
 
-% Wind expressed in NED
-Vw = 1;
-betaVw = deg2rad(135);
-rho_a = 1.247;
-cy = 0.95;
-cn = 0.15;
-A_Lw = 10 * parameters.ship.length;
+rudder_angle_vessel_2               = data_vessel_2(:, 12);
+propeller_speed_vessel_2            = data_vessel_2(:, 13) * 60;
+rudder_angle_commanded_vessel_2     = data_vessel_2(:, 14);
+propeller_speed_commanded_vessel_2  = data_vessel_2(:, 15) * 60;
 
-% Current
-Vc = 0.2;
-betaVc = deg2rad(45);
+figure();
+subplot(2, 2, 1);
+pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints);
+title("Position of Vessels");
+legend(["Target path", "Vessel 1", "Target path for Vessel 2", "Vessel 2"]);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% MAIN LOOP
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-simdata = zeros(Ns+1, length(x) + 5);       % table of simulation data
+subplot(2, 2, 2);
+plot(timespan, speed_vessel_1, timespan, speed_vessel_2);
+title("Speed");
+xlabel("Timee [s]");
+ylabel("Speed [m/s]")
+legend(["Vessel 1", "Vessel 2"]);
+grid on;
 
-for i=1:Ns+1
-    
-    t = (i-1) * h;              % time (s)
+subplot(2, 2, 3);
+plot(timespan, propeller_speed_vessel_1, ...
+     timespan, propeller_speed_commanded_vessel_1, ...
+     timespan, propeller_speed_vessel_2, ...
+     timespan, propeller_speed_commanded_vessel_2);
+title("Propeller speed");
+legend(["Vessel 1", "Commanded (Vessel 1)", "Vessel 2", "Commanded (Vessel 2)"]);
+xlabel("Time [s]");
+ylabel("Propeller speed [RPM]")
+grid on;
 
-    %% Current disturbance
+subplot(2, 2, 4);
+plot(timespan, rudder_angle_vessel_1, ...
+     timespan, rudder_angle_commanded_vessel_1, ...
+     timespan, rudder_angle_vessel_2, ...
+     timespan, rudder_angle_commanded_vessel_2);
+title("Rudder angle");
+legend(["Vessel 1", "Commanded (Vessel 1)", "Vessel 2", "Commanded (Vessel 2)"]);
+xlabel("Time [s]");
+ylabel("Angle [rad]")
+grid on;
 
-    uc = Vc * cos(betaVc - x(6)); 
-    vc = Vc * sin(betaVc - x(6)); 
-    nu_c = [uc vc 0]';
+%% Plot net data
 
-    %% Wind disturbance
+span              = sqrt((x1 - x2).^2 + (y1 - y2).^2);
+netforce_vessel_1 = sqrt(data_vessel_1(:, 7).^2 + data_vessel_1(:, 8).^2);
+netforce_vessel_2 = sqrt(data_vessel_2(:, 9).^2 + data_vessel_2(:, 10).^2);
 
-    % Wind velocity in world frame
-    uw = Vw * cos(betaVw - x(6)); 
-    vw = Vw * sin(betaVw - x(6)); 
-    
-    % These are relative to the boat's current velocity
-    u_rw = x(1) - uw;
-    v_rw = x(2) - vw;
-    V_rw = sqrt(u_rw^2 + v_rw^2); 
-    gamma_rw = -atan2(v_rw, u_rw);
-    
-    Ywind = 0.5 * rho_a * V_rw^2 * cy * sin(gamma_rw) * A_Lw;
-    Nwind = 0.5 * rho_a * V_rw^2 * cn * sin(2 * gamma_rw) * A_Lw * parameters.ship.length;
+figure();
+subplot(2, 2, 1);
+plot(timespan, span);
+title("Net span");
+xlabel("Time [s]");
+ylabel("Span [m]");
+grid on;
 
-    tau_wind = [0 Ywind Nwind]';
-
-
-    %% Guidance law
-    [current_waypoint, waypoint_index] = closest_waypoint(x(4:5), waypoints, waypoint_index);
-    
-%     radius = 1000 * (1 - 175 * K(waypoint_index));
-%
-%     [next_waypoint, found] = find_farthest_point_spanned_by_circle(radius, waypoints, waypoint_index);
-% 
-%     if true
-%         next_waypoint = waypoints(:, waypoint_index + 1)';
-%     end
-    
-    next_waypoint = waypoints(:, waypoint_index + 1)';
-
-    [e_y, pi_p] = cross_track_error(next_waypoint(1), ...
-                                    next_waypoint(2), ...
-                                    current_waypoint(1), ...
-                                    current_waypoint(2), ...
-                                    x(4), ...
-                                    x(5));
-
-    look_ahead = look_ahead_distance * (1 - 150 * K(waypoint_index));
-
-    [psi_desired, y_int_dot] = ilos_guidance(e_y, pi_p, y_int, look_ahead, kappa);
-    psi_ref = psi_desired;
-    
-    next_waypoints = [next_waypoints; next_waypoint];
-
-    %% Heading control
-    % The derivative coming from the reference model is the state
-    % derivative of the reference model
-    psi_ref_x_dot = heading_reference_model(psi_ref_x, psi_ref);
-
-    % We can the extract the desired state for heading (psi)
-    % and heading rate (r) from the reference model
-    psi_desired = psi_ref_x(1);
-    r_desired = psi_ref_x(2);
-
-    % Control law
-    e_psi = ssa(x(6) - psi_desired);
-    e_r = ssa(x(3) - r_desired);
-
-    % Now we retrieve the rudder angle command from the heading PID
-    delta_c = heading_pid(e_psi, e_r, e_psi_int, parameters.nomoto); 
-
-    % Anti-integrator windup
-    if abs(delta_c) > parameters.rudder.max_angle
-       delta_c = parameters.rudder.max_angle * sign(delta_c);
-       e_psi_int = e_psi_int - h * e_psi;
-    end
-
-    %% Speed control
-    u_desired = U_ref + 300 * K(waypoint_index);
-    e_u = x(1) - u_desired;
-
-    % Here n_c is the rps of the motor (n) commanded 
-    n_c = speed_pid(u_desired, e_u, e_u_int, parameters);
-
-    %% Ship dynamics
-    u = [delta_c n_c]';
-    [x_dot, u] = supply_model(x, u, nu_c, tau_wind, parameters);
-    
-    % Store simulation data in a table (for testing)
-    simdata(i,:) = [t x(1:3)' x(4:6)' x(7) x(8) u(1) u(2)...
-                    u_desired psi_desired r_desired];     
- 
-    %% Euler integration
-    % Make integration in batch to save some time
-    s     = [x; psi_ref_x; e_psi_int; e_u_int; y_int];
-    s_dot = [x_dot; psi_ref_x_dot; e_psi; e_u; y_int_dot];
-    
-    s = euler2(s_dot, s, h);
-
-    x = s(1:9);
-    psi_ref_x = s(10:12);
-    e_psi_int = s(13);
-    e_u_int = s(14);
-    y_int = s(15);
-
-    if mod(i, 3600 / h) == 0
-        hour = i * h / 3600;
-        fprintf("Done simulating %i/%.1f hours\n", hour, total_time / 3600.0);
-    end
-
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% PLOTS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t       = simdata(:,1);                 % s
-u       = simdata(:,2);                 % m/s
-v       = simdata(:,3);                 % m/s
-r       = (180/pi) * simdata(:,4);      % deg/s
-x       = simdata(:,5);                 % m
-y       = simdata(:,6);                 % m
-psi     = (180/pi) * simdata(:,7);      % deg
-delta   = (180/pi) * simdata(:,8);      % deg
-n       = 60 * simdata(:,9);            % rpm
-delta_c = (180/pi) * simdata(:,10);     % deg
-n_c     = 60 * simdata(:,11);           % rpm
-u_d     = simdata(:,12);                % m/s
-psi_d   = (180/pi) * simdata(:,13);     % deg
-r_d     = (180/pi) * simdata(:,14);     % deg/s
-
-
-figure(1)
-figure(gcf)
-subplot(311)
-plot(y,x,'linewidth',2); axis('equal')
-title('North-East positions (m)'); xlabel('(m)'); ylabel('(m)'); 
-subplot(312)
-plot(t,psi,t,psi_d,'linewidth',2);
-title('Actual and desired yaw angles (deg)'); xlabel('time (s)');
-legend(["Actual", "Desired"]);
-subplot(313)
-plot(t,r,t,r_d,'linewidth',2);
-title('Actual and desired yaw rates (deg/s)'); xlabel('time (s)');
-legend(["Actual", "Desired"]);
-
-
-figure(2)
-figure(gcf)
-subplot(311)
-plot(t,u,t,u_d,'linewidth',2);
-title('Actual and desired surge velocities (m/s)'); xlabel('time (s)');
-legend(["Actual", "Desired"]);
-subplot(312)
-plot(t,n,t,n_c,'linewidth',2);
-title('Actual and commanded propeller speed (rpm)'); xlabel('time (s)');
-legend(["Actual", "Desired"]);
-subplot(313)
-plot(t,delta,t,delta_c,'linewidth',2);
-title('Actual and commanded rudder angles (deg)'); xlabel('time (s)');
-legend(["Actual", "Desired"]);
-
-pathplotter(x, y, waypoints, next_waypoints');
+subplot(2, 2, 2);
+plot(timespan, netforce_vessel_1, timespan, netforce_vessel_2);
+title("Force from net on vessels");
+legend(["Vessel 1", "Vessel 2"]);
+xlabel("Time [s]");
+ylabel("Force [N]")
+grid on;
