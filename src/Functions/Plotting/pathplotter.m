78dcb79f921110355787e877f243115e3bfa15c4
Simen Gangstad <simen.gangstad@me.com>
2022-03-30 14:19:16 +0200

    Move plot function to separate folder

--- src/Functions/Plotting/pathplotter.m
+++ src/Functions/Plotting/pathplotter.m
@@ -1,4 +1,4 @@
-function pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints, guidance_waypoints)
+function pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints)
 % pathplotter(x,y) converts NE-positions to longitude and latitude coordinates
 % and plots these coordinates over a map using geoplot.
 %
@@ -9,16 +9,9 @@ function pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints, guidance_way
 % Vessel 2:
 % x2 = position in x-direction (North)         (m)
 % y2 = position in y-direction (East)          (m)
-
-% Longitude and Latitude coordinates of the waypoints
-% WP0_long = 10.3907; WP0_lat = 63.4367;
-% WP1_long = 10.3315; WP1_lat = 63.4556;
-% WP2_long = 9.8857;  WP2_lat = 63.4922;
-% WP3_long = 9.7813;  WP3_lat = 63.6490;
-% WP4_long = 9.6706;  WP4_lat = 63.6866;
-% 
-% WP_long = [WP0_long WP1_long WP2_long WP3_long WP4_long];
-% WP_lat = [WP0_lat WP1_lat WP2_lat WP3_lat WP4_lat];
+%
+% x_offset: The target x position for Vessel 2 (m)
+% y_offset: The target x position for Vessel 2 (m)
 
 start_lat = 31.137769;
 start_long = 206.744556;
@@ -26,7 +19,6 @@ start_long = 206.744556;
 l_0 = deg2rad(start_long); 
 mu_0 = deg2rad(start_lat);
 
-figure;
 [WP_long, WP_lat, ~] = flat2llh(waypoints(1, :), ...
                                 waypoints(2, :), ...
                                 zeros(1, length(waypoints(1))), ...
@@ -38,20 +30,6 @@ WP_lat = wrapTo180(rad2deg(WP_lat));
 WP_long = wrapTo360(rad2deg(WP_long));
 geoplot(WP_lat, WP_long, 'x-')
 hold on
-
-% 
-% [WP_long, WP_lat, ~] = flat2llh(guidance_waypoints(1, :), ...
-%                                 guidance_waypoints(2, :), ...
-%                                 zeros(1, length(guidance_waypoints(1))), ...
-%                                 l_0, ...
-%                                 mu_0, ...
-%                                 0);
-% WP_lat = wrapTo180(rad2deg(WP_lat));
-% WP_long = wrapTo360(rad2deg(WP_long));
-% geoplot(WP_lat, WP_long, 'yx-')
-% hold on
-
-
 
 [l1, mu1, ~] = flat2llh(x1, y1, zeros(1, length(x1)), l_0, mu_0, 0);
 l1 = wrapTo360(rad2deg(l1)); 
@@ -68,7 +46,6 @@ geoplot(mu2, l2, 'y-');
 l_offset = wrapTo360(rad2deg(l_offset)); 
 mu_offset = rad2deg(mu_offset);
 geoplot(mu_offset, l_offset, 'c-');
-hold off
 
 end
 
--- src/simulink_main.m
+++ src/simulink_main.m
@@ -9,7 +9,7 @@ parameters = construct_parameters();
 [waypoints_x, waypoints_y, waypoints_K] = generate_waypoints();
 waypoints = [waypoints_x; waypoints_y];
 
-output = sim('ship', 1 * 3600);
+output = sim('ship', 4 * 3600);
 
 %% Plot path
 
@@ -23,44 +23,32 @@ y1 = data_vessel_1(:, 2);
 % mux at the end
 data_vessel_2 = output.vessel2_data.Data;
 
+%% Plot kinematic statistics
 x2       = data_vessel_2(:, 1);
 y2       = data_vessel_2(:, 2);
 x_offset = data_vessel_2(:, 7);
 y_offset = data_vessel_2(:, 8);
 
-pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints, [])
-
-%% Plot statistics
-figure()
-
 speed_vessel_1 = sqrt(data_vessel_1(:, 4).^2 + data_vessel_1(:, 5).^2);
 speed_vessel_2 = sqrt(data_vessel_2(:, 4).^2 + data_vessel_2(:, 5).^2);
-span           = sqrt((x1 - x2).^2 + (y1 - y2).^2);
-netforce_vessel_1 = sqrt(data_vessel_1(:, 7).^2 + data_vessel_1(:, 8).^2);
-netforce_vessel_2 = sqrt(data_vessel_2(:, 9).^2 + data_vessel_2(:, 10).^2);
 
-rudder_angle_vessel_1       = data_vessel_1(:, 10);
-propeller_speed_vessel_1    = data_vessel_1(:, 11) * 60;
-rudder_angle_vessel_2       = data_vessel_2(:, 12);
-propeller_speed_vessel_2    = data_vessel_2(:, 13) * 60;
+rudder_angle_vessel_1               = data_vessel_1(:, 10);
+propeller_speed_vessel_1            = data_vessel_1(:, 11) * 60;
+rudder_angle_commanded_vessel_1     = data_vessel_1(:, 12);
+propeller_speed_commanded_vessel_1  = data_vessel_1(:, 13) * 60;
 
+rudder_angle_vessel_2               = data_vessel_2(:, 12);
+propeller_speed_vessel_2            = data_vessel_2(:, 13) * 60;
+rudder_angle_commanded_vessel_2     = data_vessel_2(:, 14);
+propeller_speed_commanded_vessel_2  = data_vessel_2(:, 15) * 60;
 
-subplot(3, 2, 1);
-plot(timespan, span);
-title("Net span");
-xlabel("Time [s]");
-ylabel("Span [m]");
-grid on;
+figure();
+subplot(2, 2, 1);
+pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints);
+title("Position of Vessels");
+legend(["Target path", "Vessel 1", "Target path for Vessel 2", "Vessel 2"]);
 
-subplot(3, 2, 2);
-plot(timespan, netforce_vessel_1, timespan, netforce_vessel_2);
-title("Force from net on vessels");
-legend(["Vessel 1", "Vessel 2"]);
-xlabel("Time [s]");
-ylabel("Force [N]")
-grid on;
-
-subplot(3, 2, 3);
+subplot(2, 2, 2);
 plot(timespan, speed_vessel_1, timespan, speed_vessel_2);
 title("Speed");
 xlabel("Timee [s]");
@@ -68,18 +56,46 @@ ylabel("Speed [m/s]")
 legend(["Vessel 1", "Vessel 2"]);
 grid on;
 
-subplot(3, 2, 4);
-plot(timespan, propeller_speed_vessel_1, timespan, propeller_speed_vessel_2);
+subplot(2, 2, 3);
+plot(timespan, propeller_speed_vessel_1, ...
+     timespan, propeller_speed_commanded_vessel_1, ...
+     timespan, propeller_speed_vessel_2, ...
+     timespan, propeller_speed_commanded_vessel_2);
 title("Propeller speed");
-legend(["Vessel 1", "Vessel 2"]);
+legend(["Vessel 1", "Commanded (Vessel 1)", "Vessel 2", "Commanded (Vessel 2)"]);
 xlabel("Time [s]");
 ylabel("Propeller speed [RPM]")
 grid on;
 
-subplot(3, 2, 5);
-plot(timespan, rudder_angle_vessel_1, timespan, rudder_angle_vessel_2);
+subplot(2, 2, 4);
+plot(timespan, rudder_angle_vessel_1, ...
+     timespan, rudder_angle_commanded_vessel_1, ...
+     timespan, rudder_angle_vessel_2, ...
+     timespan, rudder_angle_commanded_vessel_2);
 title("Rudder angle");
-legend(["Vessel 1", "Vessel 2"]);
+legend(["Vessel 1", "Commanded (Vessel 1)", "Vessel 2", "Commanded (Vessel 2)"]);
 xlabel("Time [s]");
 ylabel("Angle [rad]")
+grid on;
+
+%% Plot net statistics
+
+span           = sqrt((x1 - x2).^2 + (y1 - y2).^2);
+netforce_vessel_1 = sqrt(data_vessel_1(:, 7).^2 + data_vessel_1(:, 8).^2);
+netforce_vessel_2 = sqrt(data_vessel_2(:, 9).^2 + data_vessel_2(:, 10).^2);
+
+figure();
+subplot(2, 2, 1);
+plot(timespan, span);
+title("Net span");
+xlabel("Time [s]");
+ylabel("Span [m]");
+grid on;
+
+subplot(2, 2, 2);
+plot(timespan, netforce_vessel_1, timespan, netforce_vessel_2);
+title("Force from net on vessels");
+legend(["Vessel 1", "Vessel 2"]);
+xlabel("Time [s]");
+ylabel("Force [N]")
 grid on;
