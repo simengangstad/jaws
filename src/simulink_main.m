b8a684e2e6ac3118b8ff4eddf70e4a6a7d840a7a
Simen Gangstad <simen.gangstad@me.com>
2022-03-30 10:52:44 +0200

    Working example without strain modeling + cleanup

--- src/simulink_main.m
+++ src/simulink_main.m
@@ -3,22 +3,39 @@ load('supply.mat');
 load('supplyABC.mat');
 load('thrusters_sup.mat');
 load('wind_coeff3.mat');
+load('net_modeling.mat');
 
 parameters = construct_parameters();
 [waypoints_x, waypoints_y, waypoints_K] = generate_waypoints();
 waypoints = [waypoints_x; waypoints_y];
 
-output = sim('ship', 6 * 3600);
+output = sim('ship', 1 * 3600);
 
-x1 = output.simulation_data1.Data(:, 1);
-y1 = output.simulation_data1.Data(:, 2);
+%% Plot path
 
-data2 = reshape(output.simulation_data2.Data, 8, [])';
+timespan = output.simulation_data1.Time;
 
-x2 = data2(:, 1)';
-y2 = data2(:, 2)';
+data_vessel_1 = output.simulation_data1.Data;
+x1 = data_vessel_1(:, 1);
+y1 = data_vessel_1(:, 2);
 
-x_offset = data2(:, 7)';
-y_offset = data2(:, 8)';
+% Need to reshape for some reason for data 2, might be because we have that
+% mux at the end
+data_vessel_2 = output.simulation_data2.Data;
+
+x2       = data_vessel_2(:, 1);
+y2       = data_vessel_2(:, 2);
+x_offset = data_vessel_2(:, 7);
+y_offset = data_vessel_2(:, 8);
 
 pathplotter(x1, y1, x2, y2, x_offset, y_offset, waypoints, [])
+
+%% Plot statistics
+figure()
+
+span = sqrt((x1 - x2).^2 + (y1 - y2).^2);
+plot(timespan, span);
+title("Span");
+xlabel("Time [s]");
+ylabel("Span [m]");
+grid on;
